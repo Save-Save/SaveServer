9072619d1b201e35d144097b4b17d16a03fb1ea4
seolwonKoo <seolwon6289@naver.com>
2019-01-23 00:43:27 +0900

    [CREATE] api 라우터, db

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const apiRouter = require('./api/index');
-
-router.use('/api',apiRouter);
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
