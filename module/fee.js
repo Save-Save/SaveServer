7d32670f4e0ece376cc4cbcb4540c704f2d0ed36
seolwonKoo <seolwon6289@naver.com>
2019-01-24 01:46:12 +0900

    [UPDATE] 요금 수정, 메인 절약부분 수정

--- module/fee.js
+++ module/fee.js
@@ -12,22 +12,66 @@ module.exports = {
     },
 
     electfee : (value) => {
-        if(value<=200){
-            return(910 * value);
+        if(value<=100){
+
+            return ((value * 60.7)+410);
+
+        }else if(value<=200){
+            let temp = (100 * 60.7);
+            let temp1 = ((value-100)*125.9);
+
+            return (910 + temp + temp1);
+
+        }else if(value<=300){
+            let temp = (100 * 60.7);
+            let temp1 = (100 * 125.9) ;
+            let temp2 = ((value-200)*187.9);
+
+            return (1600 + temp+ temp1 + temp2);
+
         }else if(value<=400){
-            return(1600 * value);
+            let temp = (100 * 60.7);
+            let temp1 = (100 * 125.9);
+            let temp2 = (100 * 187.9);
+            let temp3 = ((value-300)*280.6);
+            
+            return (3850 + temp+ temp1 +temp2+temp3);
+        }else if(value<=500){
+
+            let temp = (100 * 60.7);
+            let temp1 = (100 * 125.9);
+            let temp2 = (100 * 187.9);
+            let temp3 = (100 * 280.6);
+            let temp4 = ((value-400)*417.7);
+            return (7300 + temp + temp1 + temp2 + temp3+ temp4);
         }else{
-            return(7300 * value);
+            let temp = (100 * 60.7);
+            let temp1 = (100 * 125.9);
+            let temp2 = (100 * 187.9);
+            let temp3 = (100 * 280.6);
+            let temp4 = ((value-500)*709.5);
+            return (12940 + temp+temp1+temp2+temp3+temp4);
         }
     },
 
-    saveAmount : (...args) => {
+    saveAmountElect : (...args) => {
 
-        const goalAmount = args[0];// goal이 아닌 동환경 으로 바꿔야 한다.
-        const usingAmount = args[1];
+        const lastAmount = args[0];
+        const currentAmount = args[1];
+        
+        let save = (1.18/1.12)*lastAmount;
 
-        return (goalAmount - usingAmount);
+        return (save-currentAmount);
     
+    },
+    saveAmountWater : (...args) =>{
+        const month = args[0];
+        const currentAmount = args[1];
+        if(month === 12){
+            return (21 - currentAmount);
+        }else{
+            return (22 - currentAmount);
+        }
     },
 
     electStep : (value) =>{
--- routes/api/main/index.js
+++ routes/api/main/index.js
@@ -7,14 +7,21 @@ const fee = require('../../../module/fee');
 /* GET home page. */
 router.get('/', async (req, res, next) => {
 
-
+//.subtract(1,'months')
 
     let startDay = moment().startOf('month').format('YYYY-MM-DD');
     let endDay = moment().endOf('month').format('YYYY-MM-DD');
+    let previousStart = moment().subtract(1,'months').startOf('month').format('YYYY-MM-DD');
+    let previousEnd = moment().subtract(1,'months').endOf('month').format('YYYY-MM-DD');
     let totalWater;
     let totalElect;
+    let totalElectTemp;
+    let totalWaterTemp;
+    let tempElect;
+    let tempWater;
     let savePrice;
 
+    console.log(previousStart);
     let showQuery = `
     SELECT
         elect_goal, water_goal, name , water_day, elect_day
@@ -32,6 +39,7 @@ router.get('/', async (req, res, next) => {
     WHERE election.user_idx = ? AND DATE(write_time) BETWEEN ? AND ?
     `;
     let electResult =  await db.queryParamArr(electQuery, [1,startDay,endDay]);
+    let eTempResult = await db.queryParamArr(electQuery,[1,previousStart,previousEnd]);
     console.log(electResult);
 
     
@@ -45,6 +53,7 @@ router.get('/', async (req, res, next) => {
     WHERE user_idx = ? AND DATE(write_time) BETWEEN ? AND ?
     `
     let waterResult = await db.queryParamArr(waterQuery,[1,startDay,endDay]);
+    let wTempResult = await db.queryParamArr(waterQuery,[1,previousStart,previousEnd]);
     console.log(waterResult);
 
     // 사용한 수도양이 없을 때
@@ -64,15 +73,30 @@ router.get('/', async (req, res, next) => {
     }else{
         totalElect = electResult[0].totalElect;
     }
+    // 전달 사용 전기량이 없을 때
+    if(!eTempResult){
+        totalElectTemp = 0;
+    }else{
+        totalElectTemp = eTempResult[0].totalElect;
+    }
+    if(!wTempResult){
+        totalWaterTemp = 0;
+    }else{
+        totalWaterTemp = wTempResult[0].totalWater;
+    }
+
 
     //토탈 세이브한 양
-    let tempWater = fee.saveAmount(showResult[0].water_goal,totalWater);
-    let tempElect = fee.saveAmount(showResult[0].elect_goal,totalElect);
-    savePrice = fee.electfee(tempElect)+fee.waterfee(tempWater);
+    let saveElect = fee.saveAmountElect(totalElectTemp,totalElect);
+    let saveWater = fee.saveAmountWater(moment().format('M'),totalWater);
+    savePrice =fee.electfee(saveElect)+fee.waterfee(saveWater);
+    console.log(saveElect);
+    console.log(saveWater);
 
-    console.log(tempElect);
-    console.log(tempWater);
-    console.log(savePrice);
+
+
+
+    
 
     if(savePrice<0){ // 절약된 게 없으면 0으로 처리
         savePrice = 0;
@@ -86,9 +110,9 @@ router.get('/', async (req, res, next) => {
         electDday : showResult[0].elect_day,
         electGoalPrice : fee.electfee(showResult[0].elect_goal),
         waterGoalPrice : fee.waterfee(showResult[0].water_goal),
-        waterPrice : fee.waterfee(totalWater),
-        electPrice : fee.electfee(totalElect),
-        savePrice : savePrice
+        waterPrice : fee.waterfee(totalWater).toFixed(0),
+        electPrice : fee.electfee(totalElect).toFixed(0),
+        savePrice : savePrice.toFixed(0)
     }
 
     if(!showResult){
